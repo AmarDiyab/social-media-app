7a70b17719cc2ea5f9abb715f185b3855ebd9f5c
Amar Diyab <diyabamar3043@gmail.com>
2022-09-04 23:37:57 +0530

    Added profile card

--- src/components/ProfileSide/ProfileSide.jsx
+++ src/components/ProfileSide/ProfileSide.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import LogoSearch from '../LogoSearch/LogoSearch'
+import ProfileCard from '../ProfileCard/ProfileCard'
 import './ProfileSide.css'
 
 function ProfileSide() {
   return (
     <div className='ProfileSide'>
         <LogoSearch />
+        <ProfileCard />
     </div>
   )
 }
