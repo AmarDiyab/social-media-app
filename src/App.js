ec8676e8ca1fcf8e6015530f4855267924feb7a2
Amar Diyab <diyabamar3043@gmail.com>
2022-09-28 19:48:56 +0530

    Created profile page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css"
 import Home from "./pages/Home/Home"
+import Profile from "./pages/Profile/Profile";
 
 function App() {
   return (
     <div className="App">
       <div className="Blur" style={{ top: '-18%', right: '0' }}></div>
       <div className="Blur" style={{ top: '36%', left: '-8rem'}}></div>
-      <Home />
+      {/* <Home /> */}
+      <Profile />
     </div>
   );
 }
