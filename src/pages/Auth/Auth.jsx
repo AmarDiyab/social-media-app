3a1389775acb018ab0150408b1fa8c3614e0bd89
Amar Diyab <diyabamar3043@gmail.com>
2022-11-07 12:00:39 +0530

    Added signup form

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,10 +1,44 @@
 import React from 'react'
 import './Auth.css'
-
+import Logo from '../../img/logo.png'
 function Auth() {
   return (
-    <div className='Auth'></div>
+    <div className='Auth'>
+        <div className="AuthLeft">
+            <img src={Logo} alt="" />
+            <div className="Name">
+                <h1>SOCIAL</h1>
+                <h6>Explore Ideas Throughout The World</h6>
+            </div>
+        </div>
+        <h1><SignUp /></h1>
+    </div>
   )
+}
+
+function SignUp() {
+    return (
+        <div className="AuthRight">
+            <form action="" className='infoForm AuthForm'>
+                <h3>Sign Up</h3>
+                <div>
+                    <input type="text" placeholder='First Name' className='infoInput' name='firstname' />
+                    <input type="text" placeholder='Last Name' className='infoInput' name='lastname' />
+                </div>
+                <div>
+                    <input type="text" className='infoInput' placeholder='Username' name='username ' />
+                </div>
+                <div>
+                    <input type="text" className="infoInput" placeholder='Password' name='password' />
+                    <input type="text" className="infoInput" placeholder='Confirm Password' name='confirmpass' />
+                </div>
+                <div>
+                <span className='span'>Already have an account?</span>
+                </div>
+                <button className='Button infoButton' type='submit'>Sign Up</button>                
+            </form>
+        </div>
+    )
 }
 
 export default Auth
